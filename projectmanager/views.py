9ebc4e7f35554ab99cfe6645af7ed4a39726ed08
Sagar Chavan <sagar.c.03@gmail.com>
2020-01-08 00:12:06 +0530

    cleaned up project viewset implementation

--- projectmanager/views.py
+++ projectmanager/views.py
@@ -3,19 +3,15 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Project, Membership, Task
 from .serializers import ProjectSerializer, TaskSerializer
-from .permissons import IsOwnerOrAdminProject
 from datetime import datetime
 # Create your views here.
 
 
 class ProjectViewSet(viewsets.ViewSet):
     queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [IsOwnerOrAdminProject]
 
     def list(self, request):
-        membership = Membership.objects.filter(user=request.user).first()
-        projects = Project.objects.filter(team=membership.team).all()
+        projects = self.get_team_projects(request)
         serializer = ProjectSerializer(projects, many=True)
         return Response(data=serializer.data)
 
@@ -36,17 +32,13 @@ class ProjectViewSet(viewsets.ViewSet):
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
-        membership = Membership.objects.filter(user=request.user).first()
-        projects = Project.objects.filter(team=membership.team).all()
-        instance = get_object_or_404(projects, pk=pk)
+        instance = self.get_instance(request, pk=pk)
         serializer = ProjectSerializer(instance)
         return Response(data=serializer.data)
 
     def update(self, request, pk=None):
         validated_data = request.data
-        membership = Membership.objects.filter(user=request.user).first()
-        projects = Project.objects.filter(team=membership.team).all()
-        instance = get_object_or_404(projects, pk=pk)
+        instance = self.get_instance(request, pk=pk)
         if 'endDate' in validated_data:
             validated_data['endDate'] = datetime.strptime(
                                         validated_data['endDate'],
@@ -62,17 +54,21 @@ class ProjectViewSet(viewsets.ViewSet):
         return self.update(request, pk)
 
     def destroy(self, request, pk=None):
-        membership = Membership.objects.filter(user=request.user).first()
-        projects = Project.objects.filter(team=membership.team).all()
-        instance = get_object_or_404(projects, pk=pk)
+        instance = self.get_instance(request, pk=pk)
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_team_projects(self, request):
+        membership = Membership.objects.filter(user=request.user).first()
+        return Project.objects.filter(team=membership.team).all()
+
+    def get_instance(self, request, pk=None):
+        projects = self.get_team_projects(request)
+        return get_object_or_404(projects, pk=pk)
 
 
 class TaskViewSet(viewsets.ViewSet):
     queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    permission_classes = [IsOwnerOrAdminProject]
 
     def list(self, request):
         if 'project' not in request.headers:
