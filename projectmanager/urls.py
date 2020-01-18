7e069158216b435944ab70f2e3d38d69e54d9d91
Sagar Chavan <sagar.c.03@gmail.com>
2020-01-18 06:52:54 -0800

    created project task views

--- projectmanager/urls.py
+++ projectmanager/urls.py
@@ -6,8 +6,10 @@ from rest_framework_simplejwt.views import (
 from . import views
 
 urlpatterns = [
-    path('project/', views.projects_view, name="projects"),
+    path('project/', views.projects, name="projects"),
     path('project/<int:pk>/', views.project_view, name="project"),
+    path('project/<int:pk>/tasks/', views.project_tasks, name="tasks"),
+    path('project/<int:projpk>/tasks/<int:pk>/', views.project_task_view, name="tasks"),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
 ]
--- projectmanager/views.py
+++ projectmanager/views.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 @api_view(['GET', 'POST'])
 @permission_classes([permissions.IsAuthenticated])
-def projects_view(request):
+def projects(request):
     membership = Membership.objects.filter(user=request.user).first()
     if request.method == 'GET':
         projects = Project.objects.filter(team=membership.team).all()
@@ -52,8 +52,8 @@ def project_view(request, pk=None):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     if request.method == 'PATCH' or request.method == 'PUT':
-        project.name = request.data.getitem('name', project.name)
-        project.description = request.data.getItem(
+        project.name = request.data.get('name', project.name)
+        project.description = request.data.get(
                                     'description', project.description
                                     )
         if 'endDate' in request.data:
@@ -66,102 +66,76 @@ def project_view(request, pk=None):
     serializer = ProjectSerializer(project)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-
-class TaskViewSet(viewsets.ViewSet):
-    queryset = Task.objects.all()
-
-    def list(self, request):
-        project = self.validate_and_retrive_project(request)
-
-        if isinstance(project, Response):
-            return project
-
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.IsAuthenticated])
+def project_tasks(request, pk):
+    project = Project.objects.filter(pk=pk).first()
+    membership = Membership.objects.filter(user=request.user).first()
+    if membership.team != project.team:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    
+    if request.method == 'GET':
         task = Task.objects.filter(project=project).all()
         serializer = TaskSerializer(task, many=True)
-        return Response(data=serializer.data)
-
-    def create(self, request):
-        project = self.validate_and_retrive_project(request)
-
-        if isinstance(project, Response):
-            return project
-
-        validated_data = request.data
-        validated_data['endDate'] = datetime.strptime(
-                                        validated_data['endDate'],
+    elif request.method == 'POST':
+        try:
+            data = {}
+            data['name'] = request.data['name']
+            data['description'] = request.data['description'] 
+            data['endDate'] = datetime.strptime(
+                                        request.data['endDate'],
                                         '%Y-%m-%d'
                                         ).date()
-        validated_data['startDate'] = datetime.strptime(
-                                        validated_data['startDate'],
+            data['startDate'] = datetime.strptime(
+                                        request.data['startDate'],
                                         '%Y-%m-%d'
                                         ).date()
-        validated_data['project'] = project
-
-        serializer = TaskSerializer()
-        newInstance = serializer.create(validated_data)
-        serializer = TaskSerializer(newInstance)
-        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-
-    def retrieve(self, request, pk=None):
-        instance = self.get_instance(request, pk=pk)
-        serializer = TaskSerializer(instance)
-        return Response(data=serializer.data)
-
-    def update(self, request, pk=None):
-        validated_data = request.data
-        instance = self.get_instance(request, pk=pk)
-        if 'endDate' in validated_data:
-            validated_data['endDate'] = datetime.strptime(
-                                        validated_data['endDate'],
-                                        '%Y-%m-%d'
-                                        ).date()
-        if 'startDate' in validated_data:
-            validated_data['startDate'] = datetime.strptime(
-                                        validated_data['startDate'],
-                                        '%Y-%m-%d'
-                                        ).date()
-        serializer = TaskSerializer()
-        newInstance = serializer.update(instance, validated_data)
-        serializer = TaskSerializer(newInstance)
-        return Response(data=serializer.data)
-
-    def partial_update(self, request, pk=None):
-        return self.update(request, pk)
-
-    def destroy(self, request, pk=None):
-        instance = self.get_instance(request, pk=pk)
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def validate_and_retrive_project(self, request):
-        if 'project' not in request.headers and 'project' not in request.data:
+            data['project'] = project
+            task =  Task(**data)
+            task.save()
+            serializer = TaskSerializer(task)
+        except Exception as e:
+            print(e)
             return Response(
-                        data={'message': 'project id missing from the header'},
-                        status=status.HTTP_400_BAD_REQUEST
-                        )
+                {"message": "Invalid or Missing fields"},
+                status=status.HTTP_400_BAD_REQUEST
+                )
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-        membership = Membership.objects.filter(user=request.user).first()
-        if 'project' in request.data:
-            pk = request.data['project']
-        else:
-            pk = request.headers['project']
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+@permission_classes([permissions.IsAuthenticated])
+def project_task_view(request, projpk, pk):
+    project = Project.objects.filter(pk=projpk).first()
+    membership = Membership.objects.filter(user=request.user).first()
+    if membership.team != project.team:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    
+    tasks = Task.objects.filter(project=project).all()
+    task = get_object_or_404(tasks, pk=pk)
 
-        project = Project.objects.filter(pk=pk).first()
+    if request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    if request.method == 'PUT' or request.method == 'PATCH':
+            task.name = request.data.get('name', task.name)
+            task.description = request.data.get(
+                                'description', task.description
+                                )
+            if 'endDate' in request.data:
+                task.endDate = datetime.strptime(
+                                    request.data['endDate'],
+                                    '%Y-%m-%d'
+                                    ).date()
+            if 'startDate' in request.data:
+                task.startDate = datetime.strptime(
+                                    request.data['startDate'],
+                                    '%Y-%m-%d'
+                                    ).date()
+            task.save()
 
-        if membership.team != project.team:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        return project
-
-    def get_instance(self, request, pk=None):
-        project = self.validate_and_retrive_project(request)
-
-        if isinstance(project, Response):
-            return project
-
-        task = Task.objects.filter(project=project).all()
-        return get_object_or_404(task, pk=pk)
-
+    serializer = TaskSerializer(task)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 class SubTaskViewSet(viewsets.ViewSet):
     queryset = SubTask.objects.all()
